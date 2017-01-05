25de6109eb806bde9ba9719b627c282ed30f1bc8
Gourav Shah <gs@initcron.org>
2017-01-05 20:53:05 +0530

    remove ansible code

--- code/chap7/roles/mysql/files/fetch_defaultpass.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-DEFAULT_PASS=$(grep "temporary password" /var/log/mysqld.log  | cut -d "@" -f2 | cut -d ":" -f2 | sed -e 's/^[[:space:]]*//')
-echo $DEFAULT_PASS
--- code/chap9/roles/mysql/templates/mysql_reset_pass.sql
+++
@@ -1,3 +0,0 @@
-ALTER USER 'root'@'localhost' IDENTIFIED BY "{{ mysql_root_db_pass }}";
-uninstall plugin validate_password;
-FLUSH PRIVILEGES;
